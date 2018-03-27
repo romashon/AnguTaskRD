99dff40303ee80390fd36d014179f25bf7c27546
romashon <raman.dzemianiuk@gmail.com>
2018-03-27 13:52:04 +0300

    +TSLINT
    Made changes (fix TSLINT errors)

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
-import { Person } from "./person";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Person } from './person';
 
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { Person } from "../person";
-import { PersonService } from "../person.service";
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PersonService } from '../person.service';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -11,7 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 export class PersonsComponent implements OnInit {
   persons = [];                  // all persons from responce
   selectedPerson: Person;      // person who was selected
-  ifCookieSet: boolean = false;  // flag. True when cookie is set
+  ifCookieSet = false;  // flag. True when cookie is set
   constructor(private personService: PersonService,
     private cookieService: CookieService) { }
 
@@ -23,15 +23,16 @@ export class PersonsComponent implements OnInit {
   getPersons(): void {
     this.personService.getPersons().subscribe(persons => {
     this.persons = persons;
-      if (this.persons) this.cookieCheck();
+      if (this.persons) {
+          this.cookieCheck(); }
     });
   }
 
   // If cookies aren't empty --> define the selectedPerson from cookies
   cookieCheck(): void {
     if (this.cookieService.check('personid')) {
-      for (var i = 0; i < this.persons.length; i++) {
-        if (this.persons[i].id == this.cookieService.get('personid')) {
+      for (let i = 0; i < this.persons.length; i++) {
+        if (this.persons[i].id === +this.cookieService.get('personid')) {
           this.selectedPerson = this.persons[i];
           this.ifCookieSet = true;
         }
